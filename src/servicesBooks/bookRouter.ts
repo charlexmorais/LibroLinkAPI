3e5c3b66387180a898c1d955cb4be65a9a37a5ec
charle <charlexavier100@gmail.com>
2024-02-26 19:57:55 -0300

    finalizando bookservices

---
+++ src/servicesBooks/bookRouter.ts
@@ -0,0 +1,70 @@
+// userRoutes.js
+import express from "express";
+
+import { verifyToken } from "../auth/token";
+import { app, db } from "./app.settings";
+import { BookServices } from "./bookServices";
+const bookservices =new BookServices(db)
+const bookRouter = express.Router();
+
+bookRouter.use(verifyToken);
+
+bookRouter.get("/", async (req, res) => {
+  // ... (lógica para obter todos os usuários)
+  try {
+    const books = await bookservices.getAll();
+    res.json(books);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+bookRouter.get("/:id", async (req, res) => {
+  // ... (lógica para obter um usuário por ID)
+  try {
+    const { id } = req.params;
+    const user = await bookservices.find(id);
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+app.post("/usuarios", async (req, res) => {
+  try {
+    const user = await bookservices.create(req.body);
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+bookRouter.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userDataToUpdate = req.body;
+
+    const existingUser = await bookservices.find(id);
+
+    if (!existingUser) {
+      return res.status(404).json({ message: "Usuário não encontrado" });
+    }
+
+    const updatedUser = await bookservices.update(id, userDataToUpdate);
+
+    res.json({ message: "Usuário atualizado com sucesso", user: updatedUser });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+bookRouter.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await bookservices.delete(id);
+    res.json({ message: "Usuário excluído com sucesso" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// ... (outras rotas para usuários)
+
+export default bookRouter;
--- src/servicesBooks/bookServices.ts
+++ src/servicesBooks/bookServices.ts
@@ -1,22 +1,98 @@
 import { InterfaceCrud } from "../servicesUsers/interfaces";
-import * as bcrypt from "bcrypt";
 
 interface UsersModel {
-    titulo: string;
-    autor: string;
-    categoria: string;
-    descricao: string;
-    ano_publicacao: string;
-    preco: number;
-    disponivel: boolean;
-  
-  }
+  titulo: string;
+  autor: string;
+  categoria: string;
+  descricao: string;
+  ano_publicacao: string;
+  preco: number;
+  disponivel: boolean;
+}
 
 export class BookServices implements InterfaceCrud<UsersModel> {
+  db: any; // Conexão com o banco de dados.
 
-    db: any; // Conexão com o banco de dados.
-  
-    constructor(db: any) {
-      this.db = db;
+  constructor(db: any) {
+    this.db = db;
+  }
+
+  async create(payload: Omit<UsersModel, "id">): Promise<UsersModel> {
+    try {
+      const {
+        titulo,
+        autor,
+        categoria,
+        ano_publicacao,
+        descricao,
+        preco,
+        disponivel,
+      } = payload;
+
+      const query = `
+     INSERT INTO livros ( titulo ,autor,categoria,ano_publicacao,descricao,preco,disponivel) 
+      VALUES ($1, $2, $3, $4,$5,$6,$7) RETURNING *;
+`;
+      const values = [
+        titulo,
+        autor,
+        categoria,
+        ano_publicacao,
+        descricao,
+        preco,
+        disponivel,
+      ];
+      const result = await this.db.query(query, values);
+
+      return result.rows[0];
+    } catch (error) {
+      console.error("Error while creating the user:", error);
+      throw new Error("Error while creating the user.");
     }
-}   
+  }
+  async getAll(): Promise<UsersModel[]> {
+    const result = await this.db.query("SELECT * FROM livros");
+    return result.rows as UsersModel[];
+  }
+  async find(id: string): Promise<UsersModel | null> {
+    try {
+      const result = await this.db.query(
+        "SELECT * FROM livros WHERE id = $1",
+        [id]
+      );
+      console.log("Result:", result);
+      return result.rows[0] || null;
+    } catch (error) {
+      console.error("Error in find function:", error);
+      throw error;
+    }
+  }
+  async update(id: string, payload: Partial<UsersModel>): Promise<UsersModel> {
+    const { titulo, autor, categoria, ano_publicacao, descricao, preco, disponivel } = payload;
+  
+    if (titulo || autor || categoria || ano_publicacao || descricao || preco || disponivel) {
+
+      
+  
+      let values;
+      let query;
+  
+      if (titulo || autor || categoria || ano_publicacao || descricao || preco || disponivel) {
+        
+        values = [titulo, autor, categoria, ano_publicacao, descricao, preco, disponivel, id];
+        query = "UPDATE livros SET titulo =$1, autor =$2, categoria=$3, ano_publicacao=$4, descricao=$5, preco=$6, disponivel=$7 WHERE id = $8 RETURNING *;";
+      }
+  
+      const result = await this.db.query(query, values);
+  
+      return result.rows[0];
+    } else {
+      
+      throw new Error("Nenhum campo de atualização fornecido.");
+    }
+  }
+  async delete(id: string): Promise<void> {
+    await this.db.query("DELETE FROM livros WHERE id = $1", [id]);
+  }
+  
+}
