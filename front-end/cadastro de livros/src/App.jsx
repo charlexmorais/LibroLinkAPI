6292e0b70eefd99be5d7007806eef2b5120d956e
charle <charlexavier100@gmail.com>
2024-03-07 22:14:34 -0300

    finalizando login

--- front-end/cadastro de livros/src/App.jsx
+++ front-end/cadastro de livros/src/App.jsx
@@ -1,40 +1,45 @@
 import logo from "./assets/logo.png";
-import ocultar from "./assets/ocultar.png";
 import "./global.css";
+
 function App() {
   return (
-    // container contendo todo conteudo
     <div className="container">
       <header className="header">
-        <img id="logo" src={logo} alt="logo library" />
-        <span>Por favor digite suas informações de login </span>
+        <img id="logo" src={logo} alt="Logo da biblioteca" />
+        <span>BEM-VINDO DE VOLTA!</span>
       </header>
-      <form>
+      <form className="form">
         <div className="inputcontainer">
-          <label htmlFor="email">E-mail</label>
+          <label htmlFor="email">Endereço de E-mail</label>
           <input
             type="text"
             name="email"
             id="email"
-            placeholder="joaogomes@gmail.com"
+            placeholder="Digite seu e-mail"
           />
         </div>
 
         <div className="inputcontainer">
-          <label htmlFor="password">Password</label>
+          <label htmlFor="password">Senha</label>
           <input
             type="password"
             name="password"
             id="password"
-            placeholder="************"
+            placeholder="Digite sua senha"
           />
-          <i  id="loginIcon" class="material-icons toggle-password-icon icones-login" data-form-id="login">visibility_off</i>
+          <i
+            id="loginIcon"
+            className="material-icons toggle-password-icon icones-login"
+            data-form-id="login"
+          >
+            visibility_off
+          </i>
         </div>
-        <a href="">Esqueci a senha</a>
+        <a href="#">Esqueceu sua senha?</a>
       </form>
       <button className="button">Entrar</button>
       <div className="footer">
-        <p>Você não tem uma conta? </p>
+        <p>Ainda não tem uma conta?</p>
         <a href="#">Crie a sua conta aqui</a>
       </div>
     </div>
@@ -42,3 +47,4 @@ function App() {
 }
 
 export default App;
+
